4655b5b96cfb5c29cd1847264712e0b119f8449d
Chamod Tharuka <chamodtharuka98@gmail.com>
2024-04-03 22:56:08 +0530

    Project preparation with routing --Worldwise

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+function PageNotFound() {
+  return (
+    <div>
+      <PageNav />
+      <h1>404 Error! Page not found :(</h1>
+    </div>
+  );
+}
+
+export default PageNotFound;
