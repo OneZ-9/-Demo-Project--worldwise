9eee33076c0500254397acc2e8cc49e9630d0398
Chamod Tharuka <chamodtharuka98@gmail.com>
2024-04-09 22:36:28 +0530

    Implement City component

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -6,6 +6,7 @@ const BASE_URL = "http://localhost:9000";
 function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
 
   useEffect(function () {
     async function fetchCities() {
@@ -26,11 +27,30 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
+  // Fetch Current City
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      if (!res.ok) throw new Error("There was an error loading data");
+
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch (err) {
+      alert(err);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <CitiesContext.Provider
       value={{
         cities,
         isLoading,
+        currentCity,
+        getCity,
       }}
     >
       {children}
