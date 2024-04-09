53b76357e15c12fa8e94550188fcf7ab275af556
Chamod Tharuka <chamodtharuka98@gmail.com>
2024-04-09 15:53:35 +0530

    Added CitiesContext context api to manage global states

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { useState } from "react";
 
 import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
@@ -11,60 +10,33 @@ import CityList from "./components/citylist/CityList";
 import CountryList from "./components/countrylist/CountryList";
 import City from "./components/City/City";
 import Form from "./components/form/Form";
-
-const BASE_URL = "http://localhost:9000";
+import { CitiesProvider } from "./contexts/CitiesContext";
 
 function App() {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useState(function () {
-    async function fetchCities() {
-      try {
-        setIsLoading(true);
-
-        const res = await fetch(`${BASE_URL}/cities`);
-        if (!res.ok) throw new Error("There was an error loading data");
-
-        const data = await res.json();
-        setCities(data);
-      } catch (err) {
-        alert(err);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    fetchCities();
-  }, []);
-
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Homepage />} />
-        <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Pricing />} />
-        <Route path="login" element={<Login />} />
-        <Route path="app" element={<AppLayout />}>
-          {/* Index route is the default children route when there is no chilren routes specified */}
-          {/* react-router-dom gives us the Navigate component and it will redirect us to cities as soon as index route is hit. We have to 
+    <CitiesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Homepage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="login" element={<Login />} />
+          <Route path="app" element={<AppLayout />}>
+            {/* Index route is the default children route when there is no chilren routes specified */}
+            {/* react-router-dom gives us the Navigate component and it will redirect us to cities as soon as index route is hit. We have to 
           add replace keyword to go back to the browser history stack when clicking the back button of browser */}
-          <Route index element={<Navigate replace to="cities" />} />
-          <Route
-            path="cities"
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+            <Route index element={<Navigate replace to="cities" />} />
+            <Route path="cities" element={<CityList />} />
 
-          <Route path="cities/:id" element={<City />} />
+            <Route path="cities/:id" element={<City />} />
 
-          <Route
-            path="countries"
-            element={<CountryList cities={cities} isLoading={isLoading} />}
-          />
-          <Route path="form" element={<Form />} />
-        </Route>
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+            <Route path="countries" element={<CountryList />} />
+            <Route path="form" element={<Form />} />
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </CitiesProvider>
   );
 }
 
--- src/components/citylist/CityList.jsx
+++ src/components/citylist/CityList.jsx
@@ -2,8 +2,11 @@ import styles from "./CityList.module.css";
 import Spinner from "../spinner/Spinner";
 import CityItem from "../cityitem/CityItem";
 import Message from "../message/Message";
+import { useCities } from "../../contexts/CitiesContext";
 
-function CityList({ cities, isLoading }) {
+function CityList() {
+  const { cities, isLoading } = useCities();
+
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
